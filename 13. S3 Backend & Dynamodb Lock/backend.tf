09d280a0d2492a6f703ed38ff16d8c67e112649a
iAmTopeTemi <106712999+iAmTopeTemi@users.noreply.github.com>
2022-11-09 07:34:52 -0700

    Update backend.tf

--- 13. S3 Backend & Dynamodb Lock/backend.tf
+++ 13. S3 Backend & Dynamodb Lock/backend.tf
@@ -1,10 +1,9 @@
 # Below is Backend block
 terraform {
   backend "s3" {
-    bucket = ""
-    key = ""
-    region = ""
-    profile = ""
-    dynamodb_table = ""
+    bucket = "terraform-state-file-temi"
+    key = ""temiSecondProject.tfstate
+    region = "us-east-1"
+    dynamodb_table = "terraform-table"
   }
 }
